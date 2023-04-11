c811914a90134239c073b6667781b065a6b6aba0
kwm92 <dnjsal7461@naver.com>
2023-04-11 18:16:16 +0900

    첫 커밋

---
+++ src/main/java/com/kwm/exam/demo/controller/UserHomeController.java
@@ -0,0 +1,15 @@
+package com.kwm.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserHomeController {
+
+	@RequestMapping("/user/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요";
+	}
+}
