a6764db6983b52cddd865589c8bc785c6c90036b
kwm92 <dnjsal7461@naver.com>
2023-04-14 17:17:13 +0900

    마이바티스, MySQL JDBC 드라이버 추가, 설정 파일에 DB접속정보 기입

--- src/main/java/com/kwm/exam/demo/service/ArticleService.java
+++ src/main/java/com/kwm/exam/demo/service/ArticleService.java
@@ -1,9 +1,7 @@
 package com.kwm.exam.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kwm.exam.demo.repository.ArticleRepository;
@@ -11,61 +9,30 @@ import com.kwm.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
-	// 인스턴스 변수 시작
-	private int articlesLastId;
-	private List<Article> articles;
-	// 인스턴스 변수 끝
+   private ArticleRepository articleRepository;
+   
+   public ArticleService() {
+      this.articleRepository = articleRepository;
+      
+   }
+   
+   public Article getArticle(int id) {
+      return articleRepository.getArticle(id);
+   }
+   
+   public List<Article> getArticles() {
+      return articleRepository.getArticles();
+   }
+   
+   public Article writeArticle(String title, String body) {
+      return articleRepository.writeArticle(title, body);
+   }
+   
+   public void deleteArticle(int id) {
+      articleRepository.deleteArticle(id);
+   }
 
-	public ArticleService() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-
-		makeTestData();
-	}
-	
-	public void makeTestData() {
-		for ( int i = 1; i <= 10; i++ ) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-			
-			writeArticle(title, body);
-		}
-	}
-	
-	public Article getArticle(int id) {
-		for ( Article article : articles ) {
-			if ( article.getId() == id ) {
-				return article;
-			}
-		}
-		
-		return null;
-	}
-	
-	public List<Article> getArticles() {
-		return articles;
-	}
-	
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-		
-		articles.add(article);
-		articlesLastId = id;
-		
-		return article;
-	}
-	
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-		
-		articles.remove(article);
-	}
-
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		
-		article.setTitle(title);
-		article.setBody(body);
-	}
+   public void modifyArticle(int id, String title, String body) {
+      articleRepository.modifyArticle(id, title, body);
+   }
 }
