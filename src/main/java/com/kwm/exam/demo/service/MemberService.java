260bfb839350b37d80be0b2a8f5236c332937341
kwm92 <dnjsal7461@naver.com>
2023-04-17 18:38:38 +0900

    복잡한 문장 구조를 편하게...ㅋ

--- src/main/java/com/kwm/exam/demo/service/MemberService.java
+++ src/main/java/com/kwm/exam/demo/service/MemberService.java
@@ -11,33 +11,44 @@ import com.kwm.exam.demo.vo.Member;
 public class MemberService {
 
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
-		
+
 	}
-	
+
 //	public Member getMember(String loginId) {
 //		return memberRepository.getMember(loginId);
 //	}
 //	
-	 public List<Member> getMembers() {
-	      return memberRepository.getMembers();
-	   }
-	 
+	public List<Member> getMembers() {
+		return memberRepository.getMembers();
+	}
 
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+
+		// 로그인 아이디 중복체크
 		Member oldMember = getMemberByLoginId(loginId);
-		
-		if(oldMember !=null) {
+
+		if (oldMember != null) {
 			return -1;
 		}
-		
+		oldMember = getMemberByNameAndEmail(name,email);
+		// 이름 + 이메일 중복체크
+		if (oldMember != null) {
+			return -2;
+		}
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		
+
 		return memberRepository.getLastInsertId();
 	}
-	
+
+	private Member getMemberByNameAndEmail(String name, String email) {
+		return memberRepository.getMemberByNameAndEmail(name, email);
+	}
+
+
 	private Member getMemberByLoginId(String loginId) {
 		return memberRepository.getMemberByLoginId(loginId);
 	}
@@ -45,8 +56,5 @@ public class MemberService {
 	public Member getMemberById(int id) {
 		return memberRepository.getMemberById(id);
 	}
-	
-	
-	 
-	 
+
 }
