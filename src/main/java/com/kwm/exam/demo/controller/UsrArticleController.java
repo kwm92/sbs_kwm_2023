73c035a85de1bda51de363fd2c8deb6d9f9b1920
kwm92 <dnjsal7461@naver.com>
2023-04-18 18:51:12 +0900

    ResultData에 data1Name 추가, 게시물 수정 시 권한 체크

--- src/main/java/com/kwm/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kwm/exam/demo/controller/UsrArticleController.java
@@ -25,16 +25,16 @@ public class UsrArticleController {
 	public ResultData<Article> doAdd(HttpSession httpSession, String title, String body) {
 		boolean isLogined = false;
 		int loginedMemberId = 0;
-		
-		if(httpSession.getAttribute("loginedMemberId")!=null) {
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
 			isLogined = true;
 			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
 		}
-		
-		if(isLogined ==false) {
+
+		if (isLogined == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
-		
+
 		if (Ut.empty(title)) {
 			return ResultData.from("F-1", "title(을)를 입력해주세요.");
 
@@ -44,21 +44,21 @@ public class UsrArticleController {
 			return ResultData.from("F-2", "body(을)를 입력해주세요.");
 
 		}
-		
-		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId,title, body);
+
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
 
 		int id = (int) writeArticleRd.getData1();
 
 		Article article = articleService.getArticle(id);
 
-		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
+		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), "article", article);
 	}
 
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public ResultData<List<Article>> getArticles() {
 		List<Article> articles = articleService.getArticles();
-		return ResultData.from("S-1", "게시물 리스트입니다.", articles);
+		return ResultData.from("S-1", "게시물 리스트입니다.", "articles", articles);
 	}
 
 	@RequestMapping("/usr/article/getArticle")
@@ -70,7 +70,7 @@ public class UsrArticleController {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 
-		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), "article", article);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
@@ -78,20 +78,20 @@ public class UsrArticleController {
 	public ResultData<Integer> doDelete(HttpSession httpSession, int id) {
 		boolean isLogined = false;
 		int loginedMemberId = 0;
-		
-		if(httpSession.getAttribute("loginedMemberId")!=null) {
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
 			isLogined = true;
 			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
 		}
-		
-		if(isLogined ==false) {
+
+		if (isLogined == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 
 		Article article = articleService.getArticle(id);
-		
-		if( article.getMemberId() != loginedMemberId) {
-			return ResultData.from("F-2", "로그인 후 이용해주세요.");
+
+		if (article.getMemberId() != loginedMemberId) {
+			return ResultData.from("F-2", "삭제 권한이 없습니다.");
 		}
 
 		if (article == null) {
@@ -100,21 +100,37 @@ public class UsrArticleController {
 
 		articleService.deleteArticle(id);
 
-		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id),id);
+		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id), "id", id);
 	}
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public ResultData<Integer> doModify(int id, String title, String body) {
+	public ResultData<Article> doModify(HttpSession httpSession, int id, String title, String body) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+		}
+
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 
-		articleService.modifyArticle(id, title, body);
+		ResultData actorCanModifyRd = articleService.actorCanModify(loginedMemberId, article);
 
-		return ResultData.from("S-1", Ut.f("%d번 게시물을 수정하였습니다.", id),id);
+		if (actorCanModifyRd.isFail()) {
+			return actorCanModifyRd;
+		}
+
+		return articleService.modifyArticle(id, title, body);
 	}
 	// 액션 메서드 끝
 
--- src/main/java/com/kwm/exam/demo/service/ArticleService.java
+++ src/main/java/com/kwm/exam/demo/service/ArticleService.java
@@ -11,31 +11,46 @@ import com.kwm.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
-   private ArticleRepository articleRepository;
+	private ArticleRepository articleRepository;
 
-   public ArticleService(ArticleRepository articleRepository) {
-      this.articleRepository = articleRepository;
-   }
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+	}
 
-   public Article getArticle(int id) {
-      return articleRepository.getArticle(id);
-   }
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
 
-   public List<Article> getArticles() {
-      return articleRepository.getArticles();
-   }
+	public List<Article> getArticles() {
+		return articleRepository.getArticles();
+	}
 
-   public ResultData<Integer> writeArticle(int memberId, String title, String body) {
-       articleRepository.writeArticle(memberId, title, body);
-       int id = articleRepository.getLastInsertId();
-       return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id),id);
-   }
+	public ResultData<Integer> writeArticle(int memberId, String title, String body) {
+		articleRepository.writeArticle(memberId, title, body);
+		int id = articleRepository.getLastInsertId();
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), "id", id);
+	}
 
-   public void deleteArticle(int id) {
-      articleRepository.deleteArticle(id);
-   }
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
 
-   public void modifyArticle(int id, String title, String body) {
-      articleRepository.modifyArticle(id, title, body);
-   }
+	public ResultData<Article> modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+
+		Article article = getArticle(id);
+
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 수정되었습니다", id), "article", article);
+	}
+
+	public ResultData actorCanModify(int actorId, Article article) {
+		if (article == null) {
+			return ResultData.from("F-1", "권한이 없습니다");
+		}
+
+		if (article.getMemberId() != actorId) {
+			return ResultData.from("F-2", "권한이 없습니다");
+		}
+		return ResultData.from("S-1", "수정가능합니다.");
+	}
 }
