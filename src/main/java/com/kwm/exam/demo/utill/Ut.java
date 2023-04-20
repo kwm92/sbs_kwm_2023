1afca91ca02358b29ace8a0978c16484c0c96345
kwm92 <dnjsal7461@naver.com>
2023-04-20 17:11:24 +0900

    Rq 클래스 도입으로 로그인 여부와 관련된 중복 로직 제거

--- src/main/java/com/kwm/exam/demo/utill/Ut.java
+++ src/main/java/com/kwm/exam/demo/utill/Ut.java
@@ -3,11 +3,11 @@ package com.kwm.exam.demo.utill;
 public class Ut {
 
 	public static boolean empty(Object obj) {
-		//null 체크
+		// null 체크
 		if (obj == null) {
 			return true;
 		}
-		//string 체크
+		// string 체크
 		if (obj instanceof String == false) {
 			return true;
 		}
@@ -16,8 +16,43 @@ public class Ut {
 
 		return str.trim().length() == 0;
 	}
-	public static String f(String format, Object...args) {
+
+	public static String f(String format, Object... args) {
 		return String.format(format, args);
+	}
+
+	public static String jsHistoryBack(String msg) {
+
+		return """
+				<script>
+				const msg = '%s'.trim();
+				if (msg.length > 0) {
+					alert(msg);
+				}
+				history.back();
+				</script>
+				""";
+	}
+
+	public static String jsReplace(String msg, String uri) {
+
+		if (msg == null) {
+			msg = "";
+		}
+
+		if (uri == null) {
+			uri = "";
+		}
+
+		return Ut.f("""
+				<script>
+				const msg = '%s'.trim();
+				if (msg.length > 0) {
+					alert(msg);
+				}
+				location.replace('%s');
+				</script>
+				""", msg, uri);
 	}
 
 }
