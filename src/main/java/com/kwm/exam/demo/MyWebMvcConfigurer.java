dd70b67df7d4a1cd766e01248960dc0af8bb70a5
kwm92 <dnjsal7461@naver.com>
2023-04-20 18:12:55 +0900

    NeedLogin 인터셉터 구현 및 등록

--- src/main/java/com/kwm/exam/demo/MyWebMvcConfigurer.java
+++ src/main/java/com/kwm/exam/demo/MyWebMvcConfigurer.java
@@ -6,17 +6,30 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.kwm.exam.demo.interceptor.BeforeActionInterceptor;
+import com.kwm.exam.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	// BeforeActionInterceptor 불러오기
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
+	
+	@Autowired
+	NeedLoginInterceptor needLoginInterceptor;
 
 	// 인터셉터 적용 역할
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
+		registry.addInterceptor(beforeActionInterceptor)
+				.addPathPatterns("/**")
+				.excludePathPatterns("/error")
+				.excludePathPatterns("/resource/**");
+		
+		registry.addInterceptor(needLoginInterceptor)
+				.addPathPatterns("/usr/article/write")
+				.addPathPatterns("/usr/article/doWrite")
+				.addPathPatterns("/usr/article/modify")
+				.addPathPatterns("/usr/article/doModify")
+				.addPathPatterns("/usr/article/doDelete");
 	}
-
 }
---
+++ src/main/java/com/kwm/exam/demo/interceptor/NeedLoginInterceptor.java
@@ -0,0 +1,20 @@
+package com.kwm.exam.demo.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.kwm.exam.demo.vo.Rq;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class NeedLoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		System.out.println("로그인 필요!");
+		
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+}
