d22eb38588a441a8508c59f1fb2e101fbc33aac7
kwm92 <dnjsal7461@naver.com>
2023-04-21 17:47:45 +0900

    게시물 작성의 후처리를 자바스크립트로 수행, Ut에 보를 Rq로 변경

--- src/main/java/com/kwm/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kwm/exam/demo/controller/UsrArticleController.java
@@ -25,17 +25,17 @@ public class UsrArticleController {
 	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
+	public String doWrite(HttpServletRequest req, String title, String body, String replaceUri) {
 
 		Rq rq = (Rq) req.getAttribute("rq");
 
 		if (Ut.empty(title)) {
-			return ResultData.from("F-1", "title(을)를 입력해주세요.");
+			return rq.jsHistoryBack("title(을)를 입력해주세요.");
 
 		}
 
 		if (Ut.empty(body)) {
-			return ResultData.from("F-2", "body(을)를 입력해주세요.");
+			return rq.jsHistoryBack("body(을)를 입력해주세요.");
 
 		}
 
@@ -43,9 +43,11 @@ public class UsrArticleController {
 
 		int id = (int) writeArticleRd.getData1();
 
-		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
+		if(Ut.empty(replaceUri)) {
+			replaceUri = Ut.f("../article/detail?id=%d", id);
+		}
 
-		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), "article", article);
+		return rq.jsReplace(Ut.f("%d번 글이 생성되었습니다", id), body);
 	}
 	
 	@RequestMapping("/usr/article/write")
@@ -94,7 +96,7 @@ public class UsrArticleController {
 		Rq rq = (Rq) req.getAttribute("rq");
 
 		if (rq.isLogined() == false) {
-			return Ut.jsHistoryBack("로그인 후 이용해주세요.");
+			return rq.jsHistoryBack("로그인 후 이용해주세요.");
 		}
 
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
@@ -104,12 +106,12 @@ public class UsrArticleController {
 		}
 
 		if (article.getMemberId() != rq.getLoginedMemberId()) {
-			return Ut.jsHistoryBack("삭제 권한이 없습니다.");
+			return rq.jsHistoryBack("삭제 권한이 없습니다.");
 		}
 
 		articleService.deleteArticle(id);
 
-		return Ut.jsReplace(Ut.f("%d번 게시물을 삭제하였습니다.", id), "../article/list");
+		return rq.jsReplace(Ut.f("%d번 게시물을 삭제하였습니다.", id), "../article/list");
 	}
 
 	@RequestMapping("/usr/article/modify")
@@ -144,13 +146,13 @@ public class UsrArticleController {
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
 		if (article == null) {
-			return Ut.jsHistoryBack(Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+			return rq.jsHistoryBack(Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 
 		ResultData actorCanModifyRd = articleService.actorCanModify(rq.getLoginedMemberId(), article);
 
 		if (actorCanModifyRd.isFail()) {
-			return Ut.jsHistoryBack(actorCanModifyRd.getMsg());
+			return rq.jsHistoryBack(actorCanModifyRd.getMsg());
 		}
 		
 		articleService.modifyArticle(id, title, body);
--- src/main/java/com/kwm/exam/demo/vo/Rq.java
+++ src/main/java/com/kwm/exam/demo/vo/Rq.java
@@ -17,36 +17,36 @@ public class Rq {
 	private int loginedMemberId;
 	@Getter
 	private Member loginedMember;
-	
+
 	private HttpServletRequest req;
 	private HttpServletResponse resp;
 	private HttpSession session;
-	
+
 	public Rq(HttpServletRequest req, HttpServletResponse resp, MemberService memberService) {
 		this.req = req;
 		this.resp = resp;
-		
+
 		this.session = req.getSession();
-		
+
 		boolean isLogined = false;
 		int loginedMemberId = 0;
-		
-		if ( session.getAttribute("loginedMemberId") != null) {
+
+		if (session.getAttribute("loginedMemberId") != null) {
 			isLogined = true;
 			loginedMemberId = (int) session.getAttribute("loginedMemberId");
 			loginedMember = memberService.getMemberById(loginedMemberId);
 		}
-		
+
 		this.isLogined = isLogined;
 		this.loginedMemberId = loginedMemberId;
 		this.loginedMember = loginedMember;
 	}
-	
+
 	public void printHistoryBackJs(String msg) {
-	      resp.setContentType("text/html; charset=UTF-8");
-	      print(Ut.jsHistoryBack(msg));
-	   }
-	
+		resp.setContentType("text/html; charset=UTF-8");
+		print(Ut.jsHistoryBack(msg));
+	}
+
 	public void print(String str) {
 		try {
 			resp.getWriter().append(str);
@@ -55,26 +55,33 @@ public class Rq {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void println(String str) {
 		print(str + "\n");
 	}
 
 	public void login(Member member) {
 		session.setAttribute("loginedMemberId", member.getId());
-		
+
 	}
 
 	public void logout() {
 		session.removeAttribute("loginedMemberId");
-		
+
 	}
-	
+
 	public String historyBackJsOnview(String msg) {
 		req.setAttribute("msg", msg);
 		req.setAttribute("historyBack", true);
 		return "common/js";
 	}
-	
-	
+
+	public String jsHistoryBack(String msg) {
+		return Ut.jsHistoryBack(msg);
+	}
+
+	public String jsReplace(String msg, String uri) {
+		return Ut.jsReplace(msg, uri);
+	}
+
 }
