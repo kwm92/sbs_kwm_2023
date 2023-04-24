57f42233da51dbc43c60ec77f90faf0bda19964a
kwm92 <dnjsal7461@naver.com>
2023-04-24 16:08:18 +0900

    게시물 작성시 게시판 선택

--- src/main/java/com/kwm/exam/demo/vo/Rq.java
+++ src/main/java/com/kwm/exam/demo/vo/Rq.java
@@ -2,6 +2,10 @@ package com.kwm.exam.demo.vo;
 
 import java.io.IOException;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
 import com.kwm.exam.demo.service.MemberService;
 import com.kwm.exam.demo.utill.Ut;
 
@@ -10,6 +14,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Rq {
 	@Getter
 	private boolean isLogined;
@@ -17,36 +23,38 @@ public class Rq {
 	private int loginedMemberId;
 	@Getter
 	private Member loginedMember;
-
+	
 	private HttpServletRequest req;
 	private HttpServletResponse resp;
 	private HttpSession session;
-
+	
 	public Rq(HttpServletRequest req, HttpServletResponse resp, MemberService memberService) {
 		this.req = req;
 		this.resp = resp;
-
+		
 		this.session = req.getSession();
-
+		
 		boolean isLogined = false;
 		int loginedMemberId = 0;
-
-		if (session.getAttribute("loginedMemberId") != null) {
+		
+		if ( session.getAttribute("loginedMemberId") != null) {
 			isLogined = true;
 			loginedMemberId = (int) session.getAttribute("loginedMemberId");
 			loginedMember = memberService.getMemberById(loginedMemberId);
 		}
-
+		
 		this.isLogined = isLogined;
 		this.loginedMemberId = loginedMemberId;
 		this.loginedMember = loginedMember;
+		
+		this.req.setAttribute("rq", this);
 	}
-
+	
 	public void printHistoryBackJs(String msg) {
 		resp.setContentType("text/html; charset=UTF-8");
 		print(Ut.jsHistoryBack(msg));
 	}
-
+	
 	public void print(String str) {
 		try {
 			resp.getWriter().append(str);
@@ -55,21 +63,19 @@ public class Rq {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public void println(String str) {
 		print(str + "\n");
 	}
 
 	public void login(Member member) {
 		session.setAttribute("loginedMemberId", member.getId());
-
 	}
 
 	public void logout() {
 		session.removeAttribute("loginedMemberId");
-
 	}
-
+	
 	public String historyBackJsOnview(String msg) {
 		req.setAttribute("msg", msg);
 		req.setAttribute("historyBack", true);
@@ -79,9 +85,9 @@ public class Rq {
 	public String jsHistoryBack(String msg) {
 		return Ut.jsHistoryBack(msg);
 	}
-
+	
 	public String jsReplace(String msg, String uri) {
 		return Ut.jsReplace(msg, uri);
 	}
-
+	
 }
