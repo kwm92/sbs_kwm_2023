a73bfa3ecf6130ae33abe36c13ec217692523d97
kwm92 <dnjsal7461@naver.com>
2023-04-25 14:48:38 +0900

    리스트에서 검색타입과 검색어에 따라, 총 게시물 수 계산

--- src/main/java/com/kwm/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kwm/exam/demo/repository/ArticleRepository.java
@@ -25,6 +25,6 @@ public interface ArticleRepository {
 
 	public int getLastInsertId();
 
-	public int getArticlesCount(@Param("boardId") int boardId);
+	public int getArticlesCount(int boardId, String searchKeywordTypeCode, String searchKeyword);
 
 }
